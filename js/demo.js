31a1d9e85f6006cb7477790e7b171c1619782b38
Muhammad Abdurrahman <rachman.sd@gmail.com>
2019-12-08 12:34:13 +0700

    delete ts in path js

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 var events = [
-  {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-  {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-  {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+  {'Date': new Date(2019, 12, 7), 'Title': 'Doctor appointment at 3:25pm.'},
+  {'Date': new Date(2019, 12, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
+  {'Date': new Date(2019, 12, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
 ];
 var settings = {};
 var element = document.getElementById('caleandar');
